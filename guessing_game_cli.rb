964588a39be2ff1b6ee7db94c8861b5fde38a77e
gorgeous-input-2399 <hyrum.butler3@gmail.com>
2020-08-14 01:30:12 +0000

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,19 @@
 # Code your solution here!
+require 'pry'
+
+
+def run_guessing_game
+  
+  cpu_number = rand(6) + 1
+  user_guess = gets.chomp
+  if user_guess.to_i == cpu_number
+    p"You guessed the correct number!"
+  elsif user_guess == "exit"
+    p "Goodbye!"
+  else
+    p "Sorry! The computer guessed #{cpu_number}."
+  end
+    
+end
+
+
