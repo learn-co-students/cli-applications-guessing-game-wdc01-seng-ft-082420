049ddf17b683aa1e727aefe6f71f4d0d7d179ca5
outstanding-publisher-2341 <zaxmaf@gmail.com>
2020-08-19 08:20:43 +0000

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,15 @@
 # Code your solution here!
+require 'pry'
+
+def run_guessing_game
+  magic_number = rand(6) + 1
+  puts "Guess a number from 1 to 6"
+  guess = gets.chomp
+  if guess == magic_number.to_s
+    puts "You guessed the correct number!"
+  elsif guess == "exit"
+    puts "Goodbye!"
+  else
+    puts "Sorry! The computer guessed #{magic_number}."
+  end
+end
