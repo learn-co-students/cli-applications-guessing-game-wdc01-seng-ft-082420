5976bb6a28836919743435cf32c0c078a42a54c9
Amy Sikhammountry <amy.sikhammountry@gmail.com>
2020-08-23 04:11:19 +0000

    Done.

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -15,7 +15,7 @@ input = user_input
 random_number = random_number_generator
   if input != random_number
     puts "Sorry! The computer guessed #{random_number}."
-  else input = random_number
+  elsif input = random_number
     puts "You guessed the correct number!"
   end
   if input == "exit"
